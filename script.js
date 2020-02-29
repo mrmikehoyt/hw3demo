c12132190545b22dd4ac6ef10ed48ee6ca1891f0
Michael Hoyt <m.mikehoyt@outlook.com>
2020-02-28 23:05:31 -0500

    updated comments, increased odds of number being generated

--- script.js
+++ script.js
@@ -3,33 +3,24 @@ let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //let password = generatePassword();
-  //let passwordText = document.querySelector("#password");
-  //passwordText.value = password;
+  //declaring all the arrays that I am using, some of the arrays are place holders used to randomize the arrays, and maniuplate them
   const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const specialChars = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","{","]","]",",","|",",","/","?",">","<"];
+  const specialChars = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","{","]","]","|",",","/","?",">","<"];
   const arrayvalueuppercase = uppercase.values();
   const arrayvaluelowercase = lowercase.values(); 
   const arrayvalueSpecialCharacters = specialChars.values();
   let number = ["0","1","2","3","4","5","6","7","8","9"];
   let mixedPasswordArray = []
- // let mixedcompletepass = []
   let mixedPassAllCharTypes = []
-  // let password6 = []
-  // let password8 = []
   const password4 = mixedPasswordArray.values();
   
-  //for randomizing numbers in array
+  //for randomizing numbers in array . Increasing 32 or decreasing 32 increases odds of number being listed in password
   function GeneratePassword(random){
-      for (let i = 0; i < 16; i++) {
-          // Generate a random number between 1 and 10
-          // Math.floor will round down, meaning we would get a number
-          // between 0 and 9, so we'll always add 1 to bump it up.
+      for (let i = 0; i < 32; i++) {
+ 
           let num = Math.floor(Math.random() * 8) + 1
-  
-          // Display in console
-         // console.log(num)
+
           mixedPasswordArray.push(num);
   
       }
@@ -37,80 +28,44 @@ function writePassword() {
   //for randomizing lowercase letters in array
   function GenerateLowercaseletters(random){
       for (const value of arrayvaluelowercase) {
-      
-         
-        
-    //  for (let i = 0; i < 10; i++) {
-      
       const value = random[Math.floor(Math.random() * random.length)];
-      //console.log(value);
-      mixedPasswordArray.push(value);
-     
-  // }
-  
+      mixedPasswordArray.push(value);  
       }
   }
-
-    //for randoming specialcharacters in array
+     //for randoming specialcharacters in array
     function GenerateSpecialCharacters(random){
       for (const value of arrayvalueSpecialCharacters) {
-      
-         
-        
-    //  for (let i = 0; i < 10; i++) {
-      
       const value = random[Math.floor(Math.random() * random.length)];
-      //console.log(value);
-      mixedPasswordArray.push(value);
-     
-  // }
+     mixedPasswordArray.push(value);
   
       }
   }
-  
+   //for randomizing uppercase letters in array
   function GenerateUppercaseletters(random){
       for (const value of arrayvalueuppercase) {
       
          
         
-    //  for (let i = 0; i < 10; i++) {
-      
+
       const value = random[Math.floor(Math.random() * random.length)];
-      //console.log(value);
       mixedPasswordArray.push(value);
      
-  // }
   
       }
   }
   
-
+//for creating array that is only the amount of characters that is specified and randomizing the characters in the array a final time after they are all combined
   function createArray(random){
-   
-      for (const value of password4) {
+    for (const value of password4) {
           const value = random[Math.floor(Math.random() * random.length)];
-          //console.log(value);    
-          mixedPassAllCharTypes.push(value);
+        mixedPassAllCharTypes.push(value);
           window.passwithsemicolon = mixedPassAllCharTypes.slice(0, howManyChars)
-
-          
-         //  console.log (blahblah);
         
       }
   }
   
  
- 
-  //function splice(random){
-  //     password2.slice(0, 7);
-  //}
-  
-  //function FullPassword(mixed){
-  //    var ranpass = password2.slice(0,howManyChars);
-      //console.log(ranpass);
-  //}
-  //push function value output to new array, generate 10 random characters and spit them out
-  
+ //questions that user is asked 
   let numbers = confirm ("Welcome to the password Generator. Will your password contain numbers?");
   let specialCharacters = confirm ("Will your password contain special characters?")
   let letters = confirm ("will your password contain lowercase letters?")
@@ -123,25 +78,23 @@ function writePassword() {
   let ready = confirm ("ready to generate")
   
   
-  //if (howManyChars !== var)
+  //if the user answers yes regarding numbers being in the array. the function is called to randomize numbers and include them in password
   if (numbers  === true) {
   GeneratePassword(number);
-  //need to find out how to push specialchars to new array
   }
   else {  
-  alert ("no numbers to generate") ;
+  alert ("This password will contain no numbers") ;
   }
   
+  //if the user answers yes regarding numbers being in the array. the function is called to randomize lower case letters and include them in password
   if (letters === true) {
       GenerateLowercaseletters(lowercase);
-       
-      
-      //need to find out how to push specialchars to new array
   }
   else {
-      alert ("no lowercase letters to generate");
+      alert ("This password will contain no lowercase letters");
   }
   
+  //if the user answers yes regarding numbers being in the array. the function is called to randomize upper case letters and include them in password
   if (uppercaseletters === true) {
       GenerateUppercaseletters(uppercase);
        
@@ -149,19 +102,20 @@ function writePassword() {
       //need to find out how to push specialchars to new array
   }
   else {
-      alert ("no uppercase letters to generate");
+      alert ("This password will contain no uppercase letters");
   }
-  
+    //if the user answers yes regarding numbers being in the array. the function is called to randomize special characters and include them in password
   if (specialCharacters === true) {
       GenerateSpecialCharacters(specialChars)
       //need to find out how to push specialchars to new array
   }
   else { 
-  alert ("no special characters to generate");
+  alert ("This password will contain no special characters");
   }
   
 
-  
+  //if the user clicks ok on the ready prompt. The function to randomize the password again, and reduce the characters to the password length is called. Than the password is converted
+  //has the semicolons removed from it, and the password is displayed
   if (ready === true){
       createArray(mixedPasswordArray)
       let finalpass = passwithsemicolon.join('')
@@ -170,14 +124,9 @@ function writePassword() {
       
   } 
   else {
-      alert ("theres nothing to do");
+      alert ("Looks like you are not ready to create the password. The password generator will now terminate.");
   }
 }
 
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//need to convert array to string
